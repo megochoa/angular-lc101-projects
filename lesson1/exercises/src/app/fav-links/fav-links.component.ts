2defc1df2719e4d877123790e007bc6df4e932b3
megochoa <102236786+megochoa@users.noreply.github.com>
2022-09-05 16:16:27 -0500

    Completed exercise to submit

---
+++ lesson1/exercises/src/app/fav-links/fav-links.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from "@angular/core";
+
+@Component({
+  selector: "fav-links",
+  templateUrl: "./fav-links.component.html",
+  styleUrls: ["./fav-links.component.css"],
+})
+export class FavLinksComponent implements OnInit {
+  favLinks = ["https://meh.com/", "https://www.roguefitness.com/"];
+  constructor() {}
+
+  ngOnInit() {}
+}
