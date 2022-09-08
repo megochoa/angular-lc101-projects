4d39f5875f05feb90c732c08c96d22207b98f7b5
megochoa <102236786+megochoa@users.noreply.github.com>
2022-09-07 19:39:25 -0500

    completed section for ngfor practice

--- lesson2/examples/ngfor-practice/src/app/chores/chores.component.ts
+++ lesson2/examples/ngfor-practice/src/app/chores/chores.component.ts
@@ -1,17 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-   selector: 'chores',
-   templateUrl: './chores.component.html',
-   styleUrls: ['./chores.component.css']
+  selector: "chores",
+  templateUrl: "./chores.component.html",
+  styleUrls: ["./chores.component.css"],
 })
 export class ChoresComponent implements OnInit {
-   chores = ['Empty dishwasher', 'Complete LaunchCode prep work', 'Buy groceries'];
-   todoTitles = ["Yesterday's Chores", "Today's Chores", "Tomorrow's Chores"];
+  chores = ["Buy groceries", "Clean bathroom"];
+  todoTitles = ["Yesterday's Chores", "Today's Chores", "Tomorrow's Chores"];
 
-   constructor() { }
+  constructor() {}
 
-   ngOnInit() {
-   }
-
+  ngOnInit() {}
 }
